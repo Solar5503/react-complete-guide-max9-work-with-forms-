87969bcbe8cdfb66376a673ce7a47145f17d9e88
Solar5503 <spidervvv@mail.ru>
2022-07-15 22:52:19 +0300

    add useReducer

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,28 +1,69 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialInputState = { value: '', isTouched: false };
+
+const inputStateReducer = (state, action) => {
+  if (action.type === 'INPUT')
+    return {
+      value: action.value,
+      isTouched: state.isTouched,
+    };
+  if (action.type === 'BLUR')
+    return {
+      value: state.value,
+      isTouched: true,
+    };
+  if (action.type === 'RESET') return initialInputState;
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
-  const valueChangeHandler = (e) => setEnteredValue(e.target.value);
-  const inputBlurHandler = () => setIsTouched(true);
+  const valueChangeHandler = (e) =>
+    dispatch({ type: 'INPUT', value: e.target.value });
+  const inputBlurHandler = () => dispatch({ type: 'BLUR' });
 
-  const reset = () => {
-    setEnteredValue('');
-    setIsTouched(false);
-  };
+  const reset = () => dispatch({ type: 'RESET' });
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
     inputBlurHandler,
     reset,
   };
+
+  // const [enteredValue, setEnteredValue] = useState('');
+  // const [isTouched, setIsTouched] = useState(false);
+
+  // const valueIsValid = validateValue(enteredValue);
+  // const hasError = !valueIsValid && isTouched;
+
+  // const valueChangeHandler = (e) => setEnteredValue(e.target.value);
+  // const inputBlurHandler = () => setIsTouched(true);
+
+  // const reset = () => {
+  //   setEnteredValue('');
+  //   setIsTouched(false);
+  // };
+
+  // return {
+  //   value: enteredValue,
+  //   isValid: valueIsValid,
+  //   hasError,
+  //   valueChangeHandler,
+  //   inputBlurHandler,
+  //   reset,
+  // };
 };
 
 export default useInput;
